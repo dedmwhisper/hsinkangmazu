25abfe54c0e0a62ff847749cd0a1521360f0113d
dedmwhisper <75233118+dedmwhisper@users.noreply.github.com>
2020-12-25 01:00:09 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,25 +6,21 @@ window.addEventListener('load', () =>{
   });
   
   
-/* Open the sidenav */
-function openNav() {
-  document.getElementById("mysidenav").style.width = "100%";
-  document.getElementsByClassName("navcontent")[0].style.display = "block";
-  document.getElementsByClassName("navcontent")[0].style.visibility = "visible";
-  document.html.style.overflow = 'hidden';
-}
-
-/* Close/hide the sidenav */
-function closeNav() {
-  document.getElementById("mysidenav").style.width = "0";
-  document.getElementsByClassName("navcontent")[0].style.display = "none";
-  document.html.style.overflow = 'auto';
-}
-// $("#mysidenav").on("show", function () {
-//   $("body").addClass("modal-open");
-// }).on("hidden", function () {
-//   $("body").removeClass("modal-open")
-// });
+  var body = document.getElementsByTagName("body")[0]; 
+  /* Open the sidenav */
+  function openNav() {
+    document.getElementById("mysidenav").style.width = "100%";
+    document.getElementsByClassName("navcontent")[0].style.display = "block";
+    document.getElementsByClassName("navcontent")[0].style.visibility = "visible";
+    body.style.overflow = "hidden";
+  }
+  
+  /* Close/hide the sidenav */
+  function closeNav() {
+    document.getElementById("mysidenav").style.width = "0";
+    document.getElementsByClassName("navcontent")[0].style.display = "none";
+    body.style.overflow = "auto";
+  }
   
   
   //Get the button:
@@ -61,26 +57,30 @@ var coin2 = document.getElementsByClassName("coin2")[0];
 var coin3 = document.getElementsByClassName("coin3")[0];
 var coin4 = document.getElementsByClassName("coin4")[0];
 var coin5 = document.getElementsByClassName("coin5")[0];
-var coin6 = document.getElementsByClassName("coin6")[0];
+var ans1 = document.getElementsByClassName("ans1")[0];
+var ans2 = document.getElementsByClassName("ans2")[0];
+var ans3 = document.getElementsByClassName("ans3")[0];
+var ans4 = document.getElementsByClassName("ans4")[0];
+var ans5 = document.getElementsByClassName("ans5")[0];
 coin1.onclick = function() {
   coin1.style.filter = "drop-shadow(0px 0px 10px rgb(255,255,255))";
-  // ans1.style.filter = "brightness(1)"
+  ans1.style.filter = "brightness(1)"
 }
 coin2.onclick = function() {
   coin2.style.filter = "drop-shadow(0px 0px 10px rgb(255,255,255))";
-  //ans2.style.filter = "brightness(1)"
+  ans2.style.filter = "brightness(1)"
 }
 coin3.onclick = function() {
   coin3.style.filter = "drop-shadow(0px 0px 10px rgb(255,255,255))";
-  //ans3.style.filter = "brightness(1)"
+  ans3.style.filter = "brightness(1)"
 }
 coin4.onclick = function() {
   coin4.style.filter = "drop-shadow(0px 0px 10px rgb(255,255,255))";
-  //ans4.style.filter = "brightness(1)"
+  ans4.style.filter = "brightness(1)"
 }
 coin5.onclick = function() {
   coin5.style.filter = "drop-shadow(0px 0px 10px rgb(255,255,255))";
-  //ans5.style.filter = "brightness(1)"
+  ans5.style.filter = "brightness(1)"
 }
 
 $(function(){
@@ -89,3 +89,12 @@ $(function(){
      easing: 'ease-in-sine'
  });
 })
+var change = document.getElementById("change");
+
+change.onmouseover = function(e) {
+  change.innerHTML = "新港奉天宮";
+}
+
+change.onmouseout = function(e) {
+  change.innerHTML = "金花虎爺在哪";
+}
