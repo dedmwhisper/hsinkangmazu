8ab1c61a2a5d79e5c23fcd7ebc9534032d7d0796
dedmwhisper <75233118+dedmwhisper@users.noreply.github.com>
2021-01-20 13:29:31 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -98,3 +98,22 @@ change.onmouseover = function(e) {
 change.onmouseout = function(e) {
   change.innerHTML = "金花虎爺在哪";
 }
+
+//ctamodal
+var ctamodal = document.getElementById("ctamodal");
+var btn = document.getElementById("ctabtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  ctamodal.style.display = "block";
+  body.style.overflow = "hidden";
+}
+span.onclick = function() {
+  ctamodal.style.display = "none";
+  body.style.overflow = "auto";
+}
+window.onclick = function(event) {
+  if (event.target == ctamodal) {
+    ctamodal.style.display = "none";
+    body.style.overflow = "auto";
+  }
+}
